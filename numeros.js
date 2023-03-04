e882a70ac9620c69bf44c25bd1ff38e664b60370
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2023-03-04 19:58:24 -0300

    tarea numeros

---
+++ numeros.js
@@ -0,0 +1,81 @@
+// Ejercicios
+
+let alturaCm = 179;
+let alturaM = 1.79;
+
+let pesoKG = 74.5;
+
+
+let alturaProm = Math.round(alturaM);
+console.log(alturaProm);
+
+let pesoProm = Math.floor(pesoKG);
+console.log(pesoProm);
+
+
+
+
+
+
+
+// ejemplos de la clase
+let a = 5;
+let b = 0.1;
+let c = 0.2;
+
+console.log(a);
+console.log(b);
+
+console.log(b+c);
+console.log(Math.round((0.1 + 0.2) * 100) / 100);
+
+let n1 = 3.5;
+let n2 = 4.8;
+console.log(n1 + n2);
+console.log(n1 - n2);
+console.log(n1 * n2);
+console.log(n1 / n2);
+
+
+console.log(typeof n1);
+console.log(n1);
+let n1str = a.toString();
+console.log(typeof n1str);
+
+
+let num1 = 3.3;
+let num2 = num1 * 3;
+console.log(num2);
+console.log(num2.toFixed(0));
+
+
+let e = 1839.1232456789;
+let q = 543;
+console.log(e.toFixed(2));
+console.log(q.toFixed(2));
+
+
+console.log(e.toPrecision(7));
+console.log(typeof e.toPrecision());
+
+
+let valor = new Number(3);
+console.log(valor);
+console.log(valor.valueOf());
+
+let str = new String("Hola soy un string");
+console.log(str);
+console.log(str.valueOf());
+
+
+let numero = '5.89';
+let numero2 = 17.2;
+console.log(numero + numero2);
+console.log(Number(numero) + numero2);
+
+console.log(parseInt(numero));
+console.log(parseFloat(numero));
+
+
+
+
