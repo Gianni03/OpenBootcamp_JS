9ba63d9fa02767c8a6b8a2223de5be59e58884bf
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2023-03-10 16:31:47 -0300

    ejercicio, uso de objetos

--- objetos.js
+++ objetos.js
@@ -1,10 +1,40 @@
-// EJ - 2 
+// EJ - 2
 // Crea un archivo llamado objetos.js que contenga las siguientes líneas
-
 // - Un objeto con tus datos personales (nombre, apellido, edad, altura, eresDesarrollador)
-
+const persona = {
+  nombre: "Gianni",
+  apellido: "Pasquinelli",
+  edad: 42,
+  altura: 179,
+  esDeveloper: true,
+};
 // - Una variable que obtenga tu edad a partir del objeto anterior
-
+const anios = persona.edad;
+console.log(anios);
 // - Una lista que contenga el objeto con tus datos personales y un nuevo objeto con los datos personales de tus dos mejores amig@s
+const persona2 = {
+  nombre: "Belen",
+  apellido: "Cortese",
+  edad: 43,
+  altura: 162,
+  esDeveloper: false,
+};
+const persona3 = {
+  nombre: "Ana",
+  apellido: "Cortese",
+  edad: 35,
+  altura: 182,
+  esDeveloper: false,
+};
+
+let amigos = [persona2,persona3]
+
+const listaPersonas = [ persona, ...amigos ];
+console.log(listaPersonas);
 
 // - Una nueva lista con los objetos de la lista anterior ordenados por edad, de mayor a menor
+console.log(listaPersonas)
+
+
+listaPersonas.sort((a,b)=> a.edad - b.edad);
+console.log(listaPersonas)
--- sets.js
+++ sets.js
@@ -30,3 +30,47 @@ console.log(itMiSet);
 
 const setToArr = [...miSet];
 console.log(setToArr);
+// ****************************************************************
+
+
+// Objetos
+const obj = {
+  id: 4,
+  nombre: "Juan",
+  apellido: "González",
+  isDeveloper: true,
+  libros_favoritos: ["el método", "El código da vinci"],
+  "4-juegos": [1,2,3,4]
+}
+
+console.log(obj.id);
+console.log(obj["4-juegos"])
+
+const prop = "isDeveloper";
+console.log(obj[prop]);
+
+const obj2 = obj;
+console.log(obj2);
+
+obj2.nombre = "Marcelo";
+console.log(obj2.nombre);
+console.log(obj.nombre);
+
+const obj3 = {...obj};
+
+obj.nombre = "Gianni";
+console.log(obj3.nombre);
+console.log(obj.nombre);
+
+
+// *************************************
+
+const listaPeliculas = [
+  {titulo: "Lo que el viento se llevo", fecha: 1939},
+  {titulo: "Titanic", fecha: 1997},
+  {titulo: "Moana", fecha: 2016},
+  {titulo: "El efecto mariposa", fecha: 2004},
+  {titulo: "TED", fecha: 2012}
+];
+console.log(listaPeliculas);
+
