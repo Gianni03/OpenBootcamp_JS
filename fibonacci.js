f4fd12d67ad4f9d3269bf51188a12e1c97aa26a5
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2023-03-14 18:58:35 -0300

    ejercicio fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,29 @@
+// Crea un nuevo fichero JS que contenga las siguientes líneas
+// - Una función que admita un parámetro "num", y devuelva una lista con esa cantidad de números de la secuencia de Fibonacci (Por ejemplo: num = 6 => Resultado [1, 1, 2, 3, 5, 8])
+// - Ejecuta la depuración de VSCode para visualizar la ejecución de la función
+
+function Fibonacci(num) {
+  if (num === 1) return [1]
+  if (num === 2) return [1, 1]
+  let list = [1, 1]
+  for (let i = 2; i < num; i++) {
+      list.push(list[i - 1] + list[i - 2])
+  }
+  return list
+}
+
+console.log(Fibonacci(12))
+
+
+
+
+const persona = {
+  nombre: "Gianni",
+  edad: 42
+};
+console.log(persona)
+function getDobleEdad(edad) {
+  return 2 * edad
+};
+const dobleEdad = getDobleEdad(persona.edad);
+console.log(dobleEdad);
