1e55b414d7d20b54bd0c8c3c24ae34d09ca4a5e0
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2023-03-15 14:34:47 -0300

    aprendiendo a utiliza y configurar eslint

---
+++ eslint/index.js
@@ -0,0 +1,13 @@
+const nombre = "Gianni"
+
+// esta linea tiene comillas simples, que las pusimos como no permitidas
+// para desabilitar la regla podemos sacar eslin con /*  */
+/* eslint-disable*/
+const persona2 = "Maria"
+/* eslint-enable*/
+const persona3 = "Maria"
+
+/*eslint-disable-next-line indent */
+    const string = "un string"
+
+const otro = "otro array"
