6d676f0bf61d0084764db29048bff29a514dc161
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2023-03-16 10:27:10 -0300

    ejercicio de evento y jquery

--- eventos/ejercicio/index.js
+++ eventos/ejercicio/index.js
@@ -10,3 +10,17 @@
 // - Lanza el servidor de desarrollo a través del script anterior y prueba que el botón funciona correctamente
 // - Integra jQuery a través del CDN (https://releases.jquery.com/)
 // - En el fichero index.js crea un evento click en el botón a través de jQuery, que muestre por consola "Hola, estoy utilizando jQuery"
+
+const boton = document.getElementById("btn");
+console.log(boton)
+
+boton.addEventListener("click", ()=>{
+  alert("click en el botón");
+});
+
+$(document).ready(()=>{
+
+  $(".btn").click(()=>{
+    console.log("Hola, estoy utilizando jQuery")
+  })
+})
